be84f1caf4006ce1713934e338827994d9378f5e
Michelle <46630676+CodeMichelle@users.noreply.github.com>
2021-04-10 10:27:39 -0500

    Starting day two!

---
+++ day-2/day-2-1-exercise/main.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+digit_one = two_digit_number[0]
+digit_two = two_digit_number[1]
+sum_of_digits = int(digit_one) + int(digit_two)
+print(str(digit_one) + " + " + str(digit_two) + " = " + str(sum_of_digits))
+
+
+
+
+
---
+++ day-2/day-2-start/main.py
@@ -0,0 +1,16 @@
+#Data Types
+
+#String
+print("Hello"[4])
+print("123" + "345")
+
+#Integer
+print(123 + 345)
+print(100_000_000)
+
+#Float
+3.14159
+
+#Boolean
+True
+False
