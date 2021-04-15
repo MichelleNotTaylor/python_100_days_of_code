d1e89221f3b1cad4c75dd550630495cde4a7e5ab
Michelle <shellayywow@gmail.com>
2021-04-15 05:41:53 -0500

    Forgot to commit these yesterday. Some day 4 exercises.

---
+++ day-4/day-4-1-exercise/main.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+toss = random.randint(0, 1)
+result = ""
+
+if toss == 0:
+  result = "Tails"
+else:
+  result = "Heads"
+
+print(result)
+
+
+
+
+
+
+
+
+
