6e7f86ab3f15f3b8090595e562e89578da38c2dd
Michelle <shellayywow@gmail.com>
2021-06-25 23:04:41 -0500

    Started day 8.
    Began working on the day 8 exercises, took yesterday off. :+1:

---
+++ day-8/day-8-start/main.py
@@ -0,0 +1,21 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greet():
+#   print("Hello")
+#   print("What's")
+#   print("Updog?")
+
+# greet()
+
+# def greet_with_name(name):
+#   print(f"Hello, {name}")
+
+# greet_with_name("Michelle")
+
+def greet_with(location, name):
+  print(f"Hello, {name}")
+  print(f"What is it like in {location}")
+
+greet_with(name="Michelle Taylor", location="West Des Moines")
