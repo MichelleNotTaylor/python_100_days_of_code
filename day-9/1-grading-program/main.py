652b500f7281155d3ce135ce9735bb7dbc249649
CodeMichelle <michelle.taylor@parchment.com>
2023-04-05 20:40:12 -0500

    some of day 9

---
+++ day-9/1-grading-program/main.py
@@ -0,0 +1,26 @@
+student_scores = {
+    "Gandalf": 81,
+    "Bilbo": 78,
+    "Frodo": 99,
+    "Samwise": 74,
+    "Merry": 62,
+    "Pippin": 102
+}
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
